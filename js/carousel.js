ce0d0f60544ac4926206dbede679d3171436e64f
Sahan Dissanayake <sa_han47@yahoo.com>
2015-08-02 17:50:06 +1200

    Added all the JS goodness and now the carousel can be scrolled using the radion buttons and also automatically

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,8 @@
 $(function() {
     var $carousel           = $('.carousel'),
         $carouselSlides     = $carousel.find('.carouselSlides .slide'),
-        slidesCount         = $carouselSlides.length,
+        slidesCount         = $carouselSlides.length
+        slideChangeInterval = 0,
         currentSlide        = 0; // Carousel will be started at index 0
 
     // Configuarable settings
@@ -20,11 +21,22 @@ $(function() {
             (currentSlide === $carouselSlides.length) ? currentSlide = 0 : null;
 
             $carouselSlides.eq(currentSlide).fadeIn(transitionSpeed);
+
+            // [data-slide] is not 0 indexed
+            $carousel.find('.carouselSlideRadioControls input[data-slide="'+ (currentSlide + 1) +'"]').prop('checked', 'checked');
         });
     };
+
+    $carousel.on('click', '.carouselSlideRadioControls input', function() {
+        // 
+        clearInterval(slideChangeInterval);
+        changeSlide( $(this).data('slide') );
+        slideChangeInterval = setInterval(changeSlide, slideChangeTime);
+    });
+
 
     $carouselSlides.hide();
     $carouselSlides.find(':first').show();
 
-    setInterval(changeSlide, slideChangeTime);
+    slideChangeInterval = setInterval(changeSlide, slideChangeTime);
 });
